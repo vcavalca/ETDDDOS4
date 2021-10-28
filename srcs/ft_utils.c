305effe99c526af7fc78d6041fd55c124668038b
vcavalca <61808793+vcavalca@users.noreply.github.com>
2021-10-28 16:47:01 -0300

    Made file

---
+++ srcs/ft_utils.c
@@ -0,0 +1,33 @@
+#include "../incl/ft_lib.h"
+
+void	ft_finish_with_error(MYSQL *con)
+{
+	fprintf(stderr, "%s\n", mysql_error(con));
+	mysql_close(con);
+	exit(1);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*new_s;
+	int		i;
+	int		j;
+
+	new_s = (char *)malloc((strlen(s1) + strlen(s2)) + 1);
+	if (new_s == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		new_s[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		new_s[i + j] = s2[j];
+		j++;
+	}
+	new_s[i + j] = '\0';
+	return (new_s);
+}
